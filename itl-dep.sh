e8ddb47e8afa555aea0df510a9d22e4dc1a15f1b
bondden <bond.den@gmail.com>
2016-06-23 01:13:19 +0600

    upd to esf-puml 0.6.2 (puml-8043)

---
+++ itl-dep.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npm i -S esf-puml gulp-util stream-buffers through2
+npm i -D babel-polyfill babel-preset-es2015-node6 babel-preset-stage-0 chai event-stream fs-extra gulp istanbul mocha mocha-lcov-reporter
